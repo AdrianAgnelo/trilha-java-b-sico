9f7c46348733b78d671b18f95cf63cf23cba2291
Adrian <adrianagnelo93@gmail.com>
2024-04-24 19:52:18 -0300

    commit

--- trilha-java-basico/src/MinhaClasse.java
+++ trilha-java-basico/src/MinhaClasse.java
@@ -1,5 +1,7 @@
 public class MinhaClasse {
     public static void main(String[] args) {
-        System.out.print("Olá mundo, sejam bem vindos.");
+        String meuNome = "Adrian";
+
+        int anoDeFabricacao = 2022;
     }
 }
